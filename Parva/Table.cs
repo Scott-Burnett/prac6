719f4330af4f7caa2c74e2ec19c9088ff8f0f934
Scott-Burnett <thebreakfastfactoryyay@gmail.com>
2019-09-03 08:53:18 +0200

    Table class

--- Parva/Table.cs
+++ Parva/Table.cs
@@ -11,27 +11,55 @@ namespace Parva {
     public List<int> refs;           // Line numbers where it appears
     public Entry(string name) {
       this.name = name;
-      this.refs = new List<int>();
+      this.refs = new List<int>(){1};
+    }
+    public string ToString(){
+        string s = name + ":    ";
+        foreach (int i in refs)
+            s += i + ",  ";
+        return s;
     }
   } // Entry
 
   class Table {
-    static List<Entry> list = new List<Entry>();
+    static List<Entry> entryList = new List<Entry>();
 
     public static void ClearTable() {
     // Clears cross-reference table
-    // ...
+        entryList.clear();
     } // Table.ClearTable
 
     public static void AddRef(string name, bool declared, int lineRef) {
     // Enters name if not already there, and then adds another line reference (negative
     // if at a declaration point in the original source program)
-    // ...
+        int pos = -1, i = 0;
+        foreach (Entry entry in entryList) {
+            if (entry.name == name) {
+                pos = i;
+                break;
+            }
+            i++;
+        }
+        if (pos == -1) {
+            Entry newEntry = new Entry(name);
+            if (declared)
+                newEntry.refs[0] = lineRef * -1;
+            else
+                newEntry.refs.Add(lineRef);
+            entryList.Add(newEntry);
+        }
+        else {
+            if (declared)
+                entryList[pos].refs[0] = lineRef * -1;
+            else
+                entryList[pos].refs.Add(lineRef);
+        }
     } // Table.AddRef
 
     public static void PrintTable() {
     // Prints out all references in the table (eliminate duplicates line numbers)
-    //...
+        foreach (Entry entry in entryList)
+            Console.WriteLine(entry.ToString());
     } // Table.PrintTable
 
   } // Table
